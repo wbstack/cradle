2d5eb45bf8c6928d092d7f267f6d5b8b517a9049
dena <dena@wikimedia.de>
2025-03-13 16:43:14 +0100

    add delay

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sleep 25 # give minikube some time to create and mount the secret
 if [ -r '/usr/share/ca-certificates/extra/ca.crt' ]; then
     echo 'extra/ca.crt' >> /etc/ca-certificates.conf
     update-ca-certificates
